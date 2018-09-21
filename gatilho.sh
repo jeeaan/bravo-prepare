482192147efd08679ad6509a940fbc8a51ed8acc
jean <jean.ellwanger@gmail.com>
2018-09-21 16:05:01 -0300

    Reboot na primeira execução

--- gatilho.sh
+++ gatilho.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 FIRST_BOOT="/opt/.firstboot"
+SECOND_BOOT="/opt/.secondboot"
 USUARIO_BRAVO="bravous"
 WHERE_AM_I="/opt/bravo-prepare"
 DESKTOP_PATH="$(runuser -u $USUARIO_BRAVO xdg-user-dir DESKTOP)"
@@ -7,9 +8,22 @@ MAIS="$DESKTOP_PATH/brilho_mais.desktop"
 MENOS="$DESKTOP_PATH/brilho_menos.desktop"
 TECLADO="$DESKTOP_PATH/teclado.desktop"
 
+function lightdm_is_running {
+	status=$(systemctl status lightdm | grep "running")
+	if [ -z "$status" ] && [ ! -f "SECOND_BOOT" ]
+	then
+		# Flag para rebootar o sistema APENAS primeira execucao
+		touch $SECOND_BOOT
+		reboot
+	else
+		false;
+	fi
+	return $?;
+}
+
 if [ ! -f "$FIRST_BOOT" ]
 then
-	# Rodando o script pela primeira vez
+	# Flag para conseguir dar boot e instalar o sistema
 	touch $FIRST_BOOT
 	exit 0
 fi
@@ -22,5 +36,5 @@ else
 	# Esta faltando algum icone e vou 'reconfigurar' o sistema
 	"$WHERE_AM_I"/customize.sh $USUARIO_BRAVO "$DESKTOP_PATH" $WHERE_AM_I
 	"$WHERE_AM_I"/atalhos.sh $USUARIO_BRAVO "$DESKTOP_PATH" $WHERE_AM_I
-	reboot
+	while lightdm_is_running ; do true; done
 fi
