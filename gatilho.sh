90083bb876d3eb333c280763ab32c9b465f46448
jean <jean.ellwanger@gmail.com>
2018-09-20 21:23:01 -0300

    '

--- gatilho.sh
+++ gatilho.sh
@@ -5,7 +5,6 @@ USUARIO_BRAVO="bravous"
 # DESKTOP_PATH hardcoded pq mesmo usando o xdg-user-dir o espaco da 'Area de Trabalho' tava quebrando
 #DESKTOP_PATH=$(xdg-user-dir DESKTOP)
 DESKTOP_PATH="/home/bravous/Desktop"
-ln -s /home/bravous/Área\ de\ Trabalho/ /home/bravous/Desktop
 
 MAIS="$DESKTOP_PATH/brilho_mais.desktop"
 MENOS="$DESKTOP_PATH/brilho_menos.desktop"
@@ -17,6 +16,7 @@ then
 	exit 0
 else
 	# Esta faltando algum icone e vou 'reconfigurar' o sistema
+	ln -s /home/bravous/Área\ de\ Trabalho/ $DESKTOP_PATH
 	/opt/bravo-prepare/customize.sh $USUARIO_BRAVO $DESKTOP_PATH
 	/opt/bravo-prepare/atalhos.sh $USUARIO_BRAVO $DESKTOP_PATH
 	exit 1
