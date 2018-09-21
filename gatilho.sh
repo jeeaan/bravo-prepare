2933b7790c92ad60eef22e573d177c7956dcb600
Jeancarlo <jeancarlo@cds.eb.mil.br>
2018-09-21 12:12:59 -0300

    First boot flag

--- gatilho.sh
+++ gatilho.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-
+FIRST_BOOT="/opt/.firstboot"
 USUARIO_BRAVO="bravous"
 WHERE_AM_I="/opt/bravo-prepare"
 DESKTOP_PATH="$(runuser -u $USUARIO_BRAVO xdg-user-dir DESKTOP)"
 MAIS="$DESKTOP_PATH/brilho_mais.desktop"
 MENOS="$DESKTOP_PATH/brilho_menos.desktop"
 TECLADO="$DESKTOP_PATH/teclado.desktop"
+
+if [ ! -f "$FIRST_BOOT" ]
+then
+	# Rodando o script pela primeira vez
+	touch $FIRST_BOOT
+	exit 0
+fi
 
 if [ -f "$MAIS" ] && [ -f "$MENOS" ]  && [ -f "$TECLADO" ]
 then
@@ -16,5 +23,4 @@ else
 	"$WHERE_AM_I"/customize.sh $USUARIO_BRAVO "$DESKTOP_PATH" $WHERE_AM_I
 	"$WHERE_AM_I"/atalhos.sh $USUARIO_BRAVO "$DESKTOP_PATH" $WHERE_AM_I
 	reboot
-	exit 1
 fi
